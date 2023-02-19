8be5b1181a7b26a6cce910eca94a42fc41cb0582
Iyatan Atchoro <iyatanatchoro@Iyatans-MacBook-Pro.local>
2023-02-19 12:17:28 -0500

    webview

--- src/pages/ui/shared/WebViewMessage.tsx
+++ src/pages/ui/shared/WebViewMessage.tsx
@@ -3,7 +3,8 @@ const WebViewMessage = () => {
     <div className="fixed z-50 top-0 bottom-0 left-0 right-0 flex justify-center items-center">
       <div className="bg-white p-5 rounded-md max-w-sm">
         <p className="text-center">
-          Please open this page in your device's browser to Log In or Sign up
+          Please open this page in your device&apos;s browser to Log In or Sign
+          up
         </p>
         <div className="flex justify-center mt-5"></div>
       </div>
